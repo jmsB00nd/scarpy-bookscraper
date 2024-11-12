6df3ae2069015a51a643340ce9e03eb9c11f37f5
jmsB00nd <arabhamza4349@gmail.com>
2024-11-12 06:13:50 +0100

    first spider

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,22 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
+
+        books = response.css("article.product_pod")
+        for book in books:
+            yield{
+                "name" : book.css('h3 a').attrib['title'],
+                "price" : book.css('div.product_price p.price_color::text').get(),
+                "url" : book.css('h3 a').attrib["href"]
+            }
+
+        next_page = response.css('li.next a').attrib['href']
+        if next_page is not None:
+            # Check and format next page URL correctly
+            if "catalogue/" in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else: 
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            
+            yield response.follow(next_page_url, callback=self.parse)
         pass
